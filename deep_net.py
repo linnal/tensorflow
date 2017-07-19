01e2176b191d04a31549886014683188325b465c
linnal <erindajaupi@gmail.com>
2017-07-19 15:39:33 +0200

    done deep net

--- deep_net.py
+++ deep_net.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
-from tensorflow.examples.tutorias.mnist import input_data
+from tensorflow.examples.tutorials.mnist import input_data
 
-mnist = input_data.read_data_sets("/tmp/data", one_hot=True)
+mnist = input_data.read_data_sets("./MNIST_data", one_hot=True)
 
 n_nodes_hl1 = 500
 n_nodes_hl2 = 500
@@ -10,25 +10,25 @@ n_classes = 10
 batch_size = 100 #batches of 100 of features
 
 x = tf.placeholder('float', [None, 784]) # 28*28 width*height of the input image
-x = tf.placeholder('float')
+y = tf.placeholder('float')
 
-def createHiddenLayer(previous, current):
+def create_hidden_layer(previous, current):
   return {'weights': tf.Variable(tf.random_normal([previous, current])),
-          'biases': tf.Variable(tf.random_normal(current))}
+          'biases': tf.Variable(tf.random_normal([current]))}
 
-def setRelu(previous, layer):
+def set_relu(previous, layer):
   l1 = tf.add(tf.matmul(previous, layer['weights']), layer['biases'])
   return tf.nn.relu(l1)
 
 def neural_network_model(data):
-  hidden_1_layer = createHiddenLayer(784, n_nodes_hl1)
-  hidden_2_layer = createHiddenLayer(n_nodes_hl1, n_nodes_hl2)
-  hidden_3_layer = createHiddenLayer(n_nodes_hl2, n_nodes_hl3)
-  output_layer = createHiddenLayer(n_nodes_hl3, n_classes)
+  hidden_1_layer = create_hidden_layer(784, n_nodes_hl1)
+  hidden_2_layer = create_hidden_layer(n_nodes_hl1, n_nodes_hl2)
+  hidden_3_layer = create_hidden_layer(n_nodes_hl2, n_nodes_hl3)
+  output_layer = create_hidden_layer(n_nodes_hl3, n_classes)
 
-  l1 = setRelu(data, hidden_1_layer)
-  l2 = setRelu(l1, hidden_2_layer)
-  l3 = setRelu(l2, hidden_3_layer)
+  l1 = set_relu(data, hidden_1_layer)
+  l2 = set_relu(l1, hidden_2_layer)
+  l3 = set_relu(l2, hidden_3_layer)
 
   output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
 
@@ -36,7 +36,7 @@ def neural_network_model(data):
 
 def train_neural_network(x):
   prediction = neural_network_model(x)
-  cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction, y))
+  cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
   # minimize the cost
   optimizer = tf.train.AdamOptimizer().minimize(cost)
   epochs = 10 # cycles feed forward + backprop
@@ -52,7 +52,7 @@ def train_neural_network(x):
 
       print(f'epoch: {epoch} completed out of {epochs} with loss {epoch_loss}')
 
-    correct = tf.eqaul(tf.argmax(prediction, 1), tf.argmax(y, 1))
+    correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
     accuracy_eval = accuracy.eval({x: mnist.test.images, y: mnist.test.labels})
     print(f'Accuracy is {accuracy_eval}')
