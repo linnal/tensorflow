9d2f6e7bff45a689d1bb6ec626dcceb54da5e222
linnal <erindajaupi@gmail.com>
2017-07-31 11:16:30 +0200

    add backpropagation

--- basic-rnn-tutorial/main.py
+++ basic-rnn-tutorial/main.py
@@ -1,12 +1,47 @@
 import random
 import numpy as np
+import tensorflow as tf
+from itertools import islice
 
-def generate_random_data(dimension = 5000, max_number = 20):
-  x_data = []
+batch_size = 10
+num_classes = 2
+dimension = 5000
+timestep = 7
+vocabolary = 20
+emb_dim = 4
+hidden_size = 200
+
+def generate_random_data(dimension, max_number):
+  data = []
   y_data = []
 
   for _ in range(dimension):
     temp_x_data = random.sample(range(0, max_number), max_number)
+    temp_y_data = [ 1 if x > p else 0 for (x, p) in zip(temp_x_data, [0]+temp_x_data) ]
+
+    data.append((np.array(temp_x_data), np.array(temp_y_data)))
+  return data
+
+# itera i 500 in batch di 200 ciclicamnte e fai shuffle
+def generate_batch(data, batch_size):
+  i = iter(data)
+  piece = list(islice(i, batch_size))
+  while piece:
+    yield piece
+    piece = list(islice(i, batch_size))
+
+    if not piece:
+      random.shuffle(data)
+      i = iter(data)
+      piece = list(islice(i, batch_size))
+
+
+def generate_data(dimension = 5000, vocabolary_size = 20):
+  x_data = []
+  y_data = []
+
+  for _ in range(dimension):
+    temp_x_data = random.sample(range(0, vocabolary_size), timestep)
     temp_y_data = [ 1 if x > p else 0 for (x, p) in zip(temp_x_data, [0]+temp_x_data) ]
 
     x_data.append(temp_x_data)
@@ -14,34 +49,65 @@ def generate_random_data(dimension = 5000, max_number = 20):
 
   return np.array(x_data), np.array(y_data)
 
-def generate_batch(raw_data, batch_size, num_steps):
-  raw_x, raw_y = raw_data
-  data_length = len(raw_x)
 
-  # partition raw data into batches and stack them vertically in a data matrix
-  batch_partition_length = data_length // batch_size  # 25
-  data_x = np.zeros([batch_size, batch_partition_length], dtype=np.int32) # 200x25
-  data_y = np.zeros([batch_size, batch_partition_length], dtype=np.int32)
-  for i in range(batch_size):
-    data_x[i] = raw_x[batch_partition_length * i:batch_partition_length * (i + 1)]
-    data_y[i] = raw_y[batch_partition_length * i:batch_partition_length * (i + 1)]
+x = tf.placeholder(tf.int32, [batch_size, timestep])
+y = tf.placeholder(tf.int32, [batch_size, timestep])
 
-  epoch_size = batch_partition_length // num_steps
-  for i in range(epoch_size):
-    x = data_x[:, i * num_steps:(i + 1) * num_steps]
-    y = data_y[:, i * num_steps:(i + 1) * num_steps]
-    yield (x, y)
+embeddings = tf.get_variable("embedding", [vocabolary, emb_dim])
+embedding_layer = tf.nn.embedding_lookup(embeddings, x)
 
-def generate_epochs(n, batch_size, num_steps):
-  for i in range(n):
-    yield generate_batch(generate_random_data(), batch_size, num_steps)
+x_data, y_data = generate_data(batch_size, vocabolary)
 
-def main():
-  num_steps = 5
-  batch_size = 200
-  num_classes = 2
-  state_size = 4
-  learning_rate = 0.1
+cell = tf.contrib.rnn.BasicLSTMCell(hidden_size, forget_bias=0.0, state_is_tuple=True)
+outputs = []
+state = cell.zero_state(batch_size, tf.float32)
+with tf.variable_scope("RNN"):
+  for time_step in range(timestep): # 5 steps
+    print(f'timestap {time_step}')
+    # if time_step > 0:
+    #   tf.get_variable_scope().reuse_variables()
+    print(embedding_layer[:, time_step, :])
+    (cell_output, state) = cell(embedding_layer[:, time_step, :], state)
+    outputs.append(cell_output)
+output = tf.stack(axis=1, values=outputs)
+print(f' ==> {output}')
+output = tf.reshape(output, [-1, hidden_size])
+softmax_w = tf.get_variable("softmax_w", [hidden_size, 2], dtype=tf.float32)
+softmax_b = tf.get_variable("softmax_b", [2], dtype=tf.float32)
+logits = tf.matmul(output, softmax_w) + softmax_b
+print(f' logits {logits}')
+logits = tf.reshape(logits, [batch_size, timestep,2])
 
-if __name__ == '__main__':
-  main()
+loss = tf.contrib.seq2seq.sequence_loss(
+    logits,
+    y,
+    tf.ones([batch_size, timestep], dtype=tf.float32),
+    average_across_timesteps=True,
+    average_across_batch=False
+)
+print(loss)
+cost = tf.reduce_mean(loss)
+
+optimizer = tf.train.AdamOptimizer().minimize(cost)
+epochs = 50
+tvars = tf.trainable_variables()
+
+with tf.Session() as sess:
+  sess.run(tf.global_variables_initializer())
+  for epoch in range(epochs):
+    epoch_loss = 0
+    res = sess.run([embedding_layer, optimizer, loss, cost, tvars], feed_dict={x: x_data, y: y_data})
+    epoch_loss += res[3]
+    print(f'epoch: {epoch} completed out of {epochs} with loss {epoch_loss}')
+
+  # a = sess.run([embedding_layer, loss, cost, tvars], feed_dict={x: x_data, y: y_data})
+  # print(a.shape)
+  # print(embeddings)
+  # print(outputs)
+  # print(loss.shape)
+  # print(f'loss = {a[1]}')
+  # print(f'cost = {a[2]}')
+
+  # tvars_vals = a[3]
+  for var in tvars:
+    print(var.name)
