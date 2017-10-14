9bec08aba0eba9a1e4a7b41a2d2d9bcde99c194d
linnal <erindajaupi@gmail.com>
2017-10-14 22:15:43 +0200

    added tensorboard

--- def-classification/main.py
+++ def-classification/main.py
@@ -10,7 +10,7 @@ from utils import *
 batchSize = 50
 embDim = 51
 hiddenSize = 200
-TRAINING_STEPS = 1000
+TRAINING_STEPS = 60
 NUM_LAYERS = int(sys.argv[1])
 
 datasetHandler = DatasetHandler(batchSize, "out")
@@ -51,39 +51,57 @@ loss = tf.losses.mean_squared_error(
     loss_collection=tf.GraphKeys.LOSSES,
     reduction=tf.losses.Reduction.SUM_BY_NONZERO_WEIGHTS
 )
+tf.summary.scalar("loss", loss)
 
 correct_prediction = tf.equal(y, tf.round(predictions))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+tf.summary.scalar("accuracy", accuracy)
 
 optimizer = tf.train.AdamOptimizer().minimize(loss)
 tvars = tf.trainable_variables()
 
 
 
+def train():
+  x_train, y_train = datasetHandler.nextTrainBatch()
+  train_lengths =[ getRealLength(ls, datasetHandler.eos()) for ls in x_train]
+  res = sess.run([optimizer, tvars, loss, accuracy, merged],
+    feed_dict={ x: np.array(x_train),
+                y: np.array(y_train),
+                lengths: train_lengths
+                } )
+  # print(f'train: loss={res[2]}, accuracy={res[3]}')
+
+  return res[4]
+
+def test():
+  x_test, y_test = datasetHandler.nextTestBatch()
+  test_lengths =[ getRealLength(ls, datasetHandler.eos()) for ls in x_test]
+  res = sess.run([optimizer, tvars, loss, accuracy, merged],
+    feed_dict={ x: np.array(x_test),
+                y: np.array(y_test),
+                lengths: test_lengths
+                } )
+  # print(f'test: loss={res[2]}, accuracy={res[3]}')
+
+  return res[4]
+
+
+_start = 0
 with tf.Session() as sess:
+  merged = tf.summary.merge_all()
+  train_writer = tf.summary.FileWriter( 'summaries/train', sess.graph)
+  test_writer = tf.summary.FileWriter('summaries/test')
+
   sess.run(tf.global_variables_initializer())
 
-  for i in range(0,TRAINING_STEPS):
-    print(f'\n{i}/{TRAINING_STEPS}')
-    #train
-    x_train, y_train = datasetHandler.nextTrainBatch()
-    train_lengths =[ getRealLength(ls, datasetHandler.eos()) for ls in x_train]
-    res = sess.run([optimizer, tvars, loss, accuracy],
-      feed_dict={ x: np.array(x_train),
-                  y: np.array(y_train),
-                  lengths: train_lengths
-                  } )
-    print(f'train: loss={res[2]}, accuracy={res[3]}')
+  while _start < TRAINING_STEPS:
+    _start += 1
+    print(f'\n{_start}/{TRAINING_STEPS}')
+    summary = train()
+    train_writer.add_summary(summary, _start)
 
-    if i%10 == 0:
-      #test
+    if _start%10 == 0:
       while datasetHandler.hasNextTestBatch():
-        x_test, y_test = datasetHandler.nextTestBatch()
-        test_lengths =[ getRealLength(ls, datasetHandler.eos()) for ls in x_test]
-        res = sess.run([optimizer, tvars, loss, accuracy],
-          feed_dict={ x: np.array(x_test),
-                      y: np.array(y_test),
-                      lengths: test_lengths
-                      } )
-      print(f'test: loss={res[2]}, accuracy={res[3]}')
-
+        summary = test()
+        test_writer.add_summary(summary, _start)
